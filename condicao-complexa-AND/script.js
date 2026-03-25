e333a900afa81a60b2b4887a42165c2dec831728
lucasgabmelom <lucasgabmelom@gmail.com>
2026-03-25 08:28:09 -0300

    Commit Inicial

--- condicao-complexa-AND/script.js
+++ condicao-complexa-AND/script.js
@@ -4,5 +4,5 @@ let temCNH = prompt("Possui CNH? (sim ou não)");
 if(idade >= 18 && temCNH === "sim") {
     alert("Pode dirigir");
 } else {
-        alert("Não pode dirigir")
+        alert("Não pode dirigir");
 }
